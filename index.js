12e120ff9513186d6671b828361be306d4ccb25d
Juan Carreño <jcarrenoar@unal.edu.co>
2024-03-05 20:18:06 -0500

    Fix prueba ruta /ping

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ import { config } from 'dotenv'
 
 const app = express()
 
-new pg.Pool({
-    conecctionString: process.env.DATABASE_URL_PRODUCTION
-
-    })
-
+const pool = new Pool({
+    user: 'nutriscan_db_user',
+    host: 'dpg-cnjrlgocmk4c739ik8v0-a',
+    database: 'nutriscan_db',
+    password: 'D85SsQNsI74qwGGrKNsk70UVstWa5c6I',
+    port: 5432, // Your database port
+  });
 app.get('/', (req,res) => {
     res.send('Hello world')
 })
 
 app.get('/ping', async (req,res) => {
+    res.send('Hello world')
     const result = await pool.query('SELECT NOW()')
     return res.json(result.rows[0])
 })
