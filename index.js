e102e8c31cd5f566f930f3040466f5c4c17ea75c
Juan Carreño <jcarrenoar@unal.edu.co>
2024-03-05 20:21:52 -0500

    Fix test /ping

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 import express from "express";
 import pg from 'pg'
-const { Pool } = require('pg');
 import { config } from 'dotenv'
 
 const app = express()
-
-const pool = new Pool({
-    user: 'nutriscan_db_user',
-    host: 'dpg-cnjrlgocmk4c739ik8v0-a',
-    database: 'nutriscan_db',
-    password: 'D85SsQNsI74qwGGrKNsk70UVstWa5c6I',
-    port: 5432, // Your database port
-  });
 
 app.get('/', (req,res) => {
     res.send('Hello world')
 })
 
 app.get('/ping', async (req,res) => {
-    res.send('Hello world')
-    const result = await pool.query('SELECT NOW()')
-    return res.json(result.rows[0])
+    res.send('Hello world ping')
 })
 
 app.listen(3000)
