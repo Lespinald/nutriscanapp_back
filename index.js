a766bda1135d32dc4dc78020048c63de05b96c2b
Juan Carreño <jcarrenoar@unal.edu.co>
2024-03-05 22:21:49 -0500

    Fix try

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-import express from "express";
-import pg from 'pg'
-import { config } from 'dotenv'
-import { pool } from './config'
+import { Pool } from "pg";
 
-const app = express()
+// Configura la conexión a la base de datos en Render
+const pool = new Pool({
+  user: 'nutriscan_db_user',
+  host: 'nutriscanapp-back.onrender.com',
+  database: 'nutriscan_db',
+  password: 'tu_contraseña_de_la_base_de_datos',
+  port: 5432,
+  ssl: true // Asegúrate de que esto esté configurado correctamente si Render requiere SSL
+});
 
-app.get('/', (req,res) => {
-    res.send('Hello world')
-})
-
-app.get('/ping', async (req,res) => {
-    const result = await pool.query('SELECT NOW()')
-    return res.json(result.rows[0])
-})
-
-app.listen(3000)
-console.log("server port ", 3000)
+// Realiza la consulta 'SELECT NOW()'
+pool.query('SELECT NOW()', (error, result) => {
+  if (error) {
+    console.error('Error al ejecutar la consulta:', error);
+  } else {
+    console.log('Resultado de la consulta:', result.rows);
+  }
+  
+  // Cierra la conexión con la base de datos
+  pool.end();
+});
